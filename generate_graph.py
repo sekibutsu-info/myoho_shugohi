f9f6f984d654c129b9a571bac8655b42d7068b28
Midori IT, LLC. <91418411+sekibutsu-info@users.noreply.github.com>
2025-12-24 17:49:53 +0900

    保留・未定義をグラフに含めない

--- generate_graph.py
+++ generate_graph.py
@@ -11,7 +11,6 @@ years = list(range(min_year,max_year))
 
 count_narrow = [0] * len(years)
 count_wide = [0] * len(years)
-count_undef = [0] * len(years)
 
 for index, row in df.iterrows():
     if not pd.isnull(row['造立年']):
@@ -20,26 +19,19 @@ for index, row in df.iterrows():
             count_narrow[idx] += 1
         elif row['分類'] == '広義':
             count_wide[idx] += 1
-        elif row['分類'] == '保留':
-            count_undef[idx] += 1        # 保留は未定義に含める
-        elif row['分類'] == '未定義':
-            count_undef[idx] += 1
 
 # グラフ描画
 plt.figure(figsize=(10,6))
 plt.bar(years, count_narrow)
 plt.bar(years, count_wide, bottom=count_narrow)
-offsets = []
 y_maxs = []
-for x, y, z in zip(count_narrow, count_wide, count_undef):
-  offsets.append(x + y)
-  y_maxs.append(x + y + z)
-plt.bar(years, count_undef, bottom=offsets)
+for x, y in zip(count_narrow, count_wide):
+  y_maxs.append(x + y)
 y_max = max(y_maxs) + 1
 plt.ylim(0, y_max)
 plt.ylabel('造立数')
 plt.xlabel('造立年')
-plt.legend(['狭義', '広義', '保留/未定義'])
+plt.legend(['狭義', '広義'])
 plt.tight_layout()
 
 # 画像保存
